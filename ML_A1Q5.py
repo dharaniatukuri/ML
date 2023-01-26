a39cef9a10dc3940223bd2a9715a5191322e7a44
Manisha3196 <123271433+Manisha3196@users.noreply.github.com>
2023-01-25 20:44:53 -0600

    Add files via upload

---
+++ ML_A1Q5.py
@@ -0,0 +1,11 @@
+r=30
+#calculating the area of circle
+_area_of_circle=3.14*(r**2)
+print("area of circle :",_area_of_circle)
+#calculating the circumference of circle
+_circum_of_circle=2*3.17*r
+print("circumference of circle :",_circum_of_circle)
+#taking radius as user input and calculating the area
+n=int(input())
+area=3.14*(n**2)
+print("Area of circle with user input :",area)
