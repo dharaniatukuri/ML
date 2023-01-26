f6e3b06c317ff6354fb5887bc512f47c08f0dd7a
Manisha3196 <123271433+Manisha3196@users.noreply.github.com>
2023-01-25 20:45:28 -0600

    Add files via upload

---
+++ ML_A1Q8.py
@@ -0,0 +1,8 @@
+#assigned variable radius
+radius=10
+print(f'radius=10')
+#assigned area of circle formula
+area=3.14*radius**2
+print(f'area=3.14 * radius ** 2')
+#print the output using string format
+print("The area of circle with radius {} is {} meters square".format(radius,area))
